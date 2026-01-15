62d62f2d54a4d46a216b6ff6fe3a2a0243d2482f
Zeph <55712226+ZephMK2@users.noreply.github.com>
2026-01-16 00:38:46 +0100

    pixel art assets

--- Power up game/Assets/Scripts/PlayerShooting.cs
+++ Power up game/Assets/Scripts/PlayerShooting.cs
@@ -25,7 +25,7 @@ public class PlayerShooting : MonoBehaviour
         {
             neededTime -= Time.deltaTime;
         }
-        else if(neededTime <= 0 && canShoot = false)
+        else if(neededTime <= 0 && canShoot == false)
         {
             canShoot = true;
         }
