d5d60b7c056c0111015d4e387e03e2d5de81771a
Zeph <55712226+ZephMK2@users.noreply.github.com>
2026-01-19 00:35:59 +0100

    rawr

--- Power up game/Assets/Scripts/MainMenu.cs
+++ Power up game/Assets/Scripts/MainMenu.cs
@@ -3,9 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject tutorialChecker;
+    public GameObject normalButtons;
+    public void CheckTutorial()
+    {
+        tutorialChecker.SetActive(true);
+        normalButtons.SetActive(false);
+    }    
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void StartTutorial()
+    {
+        SceneManager.LoadScene("TutorialScene");
     }
 
     public void StopGame()
